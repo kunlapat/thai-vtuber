cbf84e60d8e7258b0083bbd893fcb8248afb0131
Vatunyoo Suwannapisit <ker.los@hotmail.com>
2025-09-22 11:21:21 +0700

    add gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { QueryProvider } from "@/components/QueryProvider";
 import { AppLayout } from "@/components/AppLayout";
+import { GoogleTagManager } from '@next/third-parties/google'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -13,6 +14,8 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+const googleTagManagerId = process.env.NEXT_PUBLIC_GTAG_ID;
 
 export const metadata: Metadata = {
   title: "Thai VTuber",
@@ -42,6 +45,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      {googleTagManagerId && (
+        <GoogleTagManager gtmId={googleTagManagerId} />
+      )}
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
